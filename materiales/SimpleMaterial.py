05b2c169909b1b0078d307dcb69234328e72c737
Olmichu22 <olmichu33@gmail.com>
2025-03-12 19:06:21 +0100

    Añade la clase 'MixedComplexMaterial' para gestionar materiales complejos y su índice de refracción efectivo. Actualiza 'SimpleMaterial' y 'optica.py' para integrar nuevos cálculos.

--- materiales/SimpleMaterial.py
+++ materiales/SimpleMaterial.py
@@ -1,4 +1,6 @@
-from propiedades.IndicesRefraccion import nCte
+from propiedades.IndicesRefraccion import nCte, nComplex
+from utilidades.optica import indiceEfectivo
+
 class SimpleMaterial():
   
   def __init__(self, n = nCte(1.44)):
@@ -14,6 +16,42 @@ class SimpleMaterial():
   
   def __repr__(self):
     return "SimpleMaterial(" + str(self.indice_refraccion) + ")"
+  
+  def __eq__(self, other):
+    return self.indice_refraccion == other.indice_refraccion
+  
+
+class MixedComplexMaterial():
+  
+  def __init__(self, n = [], fs = []):
+    """Constructor de la clase MixedMaterial.
+
+    Args:
+        n (list, Function): Lista de funciones que calculan índices de refracción dada f.
+    """
+    self.indices_refraccion = n
+    self.indice_refraccion = self.compose()
+    self.fs = fs
+  
+  def compose(self):
+    """Calcula el índice de refracción promedio de los índices de refracción de la lista.
+    
+    Returns:
+        Function: Función que calcula el índice de refracción promedio.
+    """
+    self.indice_promedio = self.indices_refraccion[0]
+    self.cum_fs = self.fs[0]
+    for i, n in enumerate(1, self.indices_refraccion):
+      nr, ni = indiceEfectivo(self.indice_promedio.nc, n.nc, self.cum_fs, self.fs[i])
+      self.indice_promedio = nComplex(nr, ni)
+      self.cum_fs += self.fs[i]
+    return self.indice_promedio
+  
+  def __str__(self):
+    return "Material con " + str(self.indice_refraccion)
+  
+  def __repr__(self):
+    return "MixedComplexMaterial(" + str(self.indice_refraccion) + ")"
   
   def __eq__(self, other):
     return self.indice_refraccion == other.indice_refraccion
